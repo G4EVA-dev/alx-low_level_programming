992c3208a83cddce756e868ca7558187fc2359bd
G4EVA-dev <tanzeglenn@gmail.com>
2022-10-21 21:46:46 -0700

    multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* mul - multiply two given numbers
+* @a: first int
+* @b: sexond int
+* Return: multiplication of a and b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
