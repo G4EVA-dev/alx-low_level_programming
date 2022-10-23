93208c443eebba6dbe292406c0648023df08604c
G4EVA-dev <tanzeglenn@gmail.com>
2022-10-22 18:18:16 -0700

    print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @n: Size of the triangle
+ */
+void print_triangle(int n)
+{
+	int h, tri;
+
+	if (n > 0)
+	{
+		for (h = 1; h <= n; h++)
+		{
+			for (tri = n - h; tri > 0; tri--)
+				_putchar(' ');
+
+			for (tri = 0; tri < h; tri++)
+				_putchar('#');
+
+			if (h == n)
+				comtinue;
+
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
