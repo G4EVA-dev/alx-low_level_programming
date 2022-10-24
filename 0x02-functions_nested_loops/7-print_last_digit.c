1ea08ce6d14b8ef7b3148c2dd0501f3e4ec3a863
G4EVA-dev <tanzeglenn@gmail.com>
2022-10-23 18:46:27 -0700

    print last digit fixed

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,10 +7,12 @@
  */
 int print_last_digit(int n)
 {
-	int ld = n % 100;
+	int ld = n % 10;
 
-	if (ld <= 0)
-		ld += -1;
+	if (ld < 0)
+		ld *= -1;
 
-	return (ld);
+	_putchar(ld + '0');
+
+	return (0);
 }
