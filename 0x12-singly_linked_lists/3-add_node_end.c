02826fca2961002f5c482d4b285c6925fe4a11bd
G4EVA-dev <tanzeglenn@gmail.com>
2023-04-26 11:37:42 -0700

    add node end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new node at the end of a linked list
+ * @head: Doubles the pointer to the list_t list
+ * @str: The string to put in the new node
+ *
+ * Return: address of the new element, or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *_new;
+	list_t *_temp = *head;
+	unsigned int length = 0;
+
+	while (str[length])
+		length++;
+
+	_new = malloc(sizeof(list_t));
+	if (!_new)
+		return (NULL);
+
+	_new->str = strdup(str);
+	_new->length = length;
+	_new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = _new;
+		return (_new);
+	}
+
+	while (_temp->next)
+		_temp = _temp->next;
+
+	_temp->next = _new;
+
+	return (_new);
+}
