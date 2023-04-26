d7d7bb1e7fcb55ce572848b187969e5acf62a4b3
G4EVA-dev <tanzeglenn@gmail.com>
2023-04-26 11:27:16 -0700

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,23 +5,23 @@
 /**
  * add_node - Add New node at beginning of a linked list
  * @Head: Doubles the pointer to the list_t list
- * @strn: New string to be added in the node
+ * @str: New string to be added in the node
  *
  * Return: The address of the new element, or NULL if it fails
  */
-list_t *add_node(list_t **Head, const char *strn)
+list_t *add_node(list_t **Head, const char *str)
 {
 	list_t *_New;
 	unsigned int length = 0;
 
-	while (strn[length])
+	while (str[length])
 		length++;
 
 	_New = malloc(sizeof(list_t));
 	if (!_New)
 		return (NULL);
 
-	_New->strn = strdup(strn);
+	_New->str = strdup(str);
 	_New->length = length;
 	_New->next = (*Head);
 	(*Head) = _New;
