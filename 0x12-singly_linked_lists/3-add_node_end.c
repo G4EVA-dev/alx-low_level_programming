c379ee09909fc535ba69a5f77a32b2096f49c526
G4EVA-dev <tanzeglenn@gmail.com>
2023-04-26 11:51:26 -0700

    free list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -7,7 +7,7 @@
  * @head: Doubles the pointer to the list_t list
  * @str: The string to put in the new node
  *
- * Return: address of the new element, or NULL if it failed
+ * Return: address of the new element, or NULL if failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
