68eb08cee37fd9aba2facfdaca0cf3bd5518a601
G4EVA-dev <tanzeglenn@gmail.com>
2023-04-26 10:59:27 -0700

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - It Prints all the elements of a linked list as shown
+ * @H: Pointer to the list_t list to print linked list.
+ *
+ * Return: the number of nodes printed are shown.
+ */
+size_t print_list(const list_t *H)
+{
+	size_t sz = 0;
+
+	while (H)
+	{
+		if (!H->strn)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", H->length, H->strn);
+		H = H->nextPt;
+		sz++;
+	}
+	return (sz);
+}
